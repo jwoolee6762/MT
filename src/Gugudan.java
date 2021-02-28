ba0679250a1a55e2ba4e9931f43f37fca7bcd3e3
iseung-u <iseung-u@192.168.0.4>
2021-02-28 16:21:22 +0900

    Conditional

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -3,17 +3,35 @@ import java.util.Scanner;
 
 public class Gugudan {
 	public static void main(String[] args) {
+		// 8, 9단
+		System.out.println("구구단 중 출력할 단은? : ");
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt();
+		System.out.println("사용자가 입력한 값 : " + number);
+		
+		if (number < 2) {
+			System.out.println("값을 잘못 입력했습니다.");
+		} else if (number > 9) {
+			System.out.println("값을 잘못 입력했습니다.");
+		} else {
+			for(int i = 1; i < 10; i++) {
+				System.out.println(number * i);
+			}
+		} // 조건문 이해하자.
+		
+		
+		
 		// 6단 
-		int i = 1;
-		while(i < 10) {
-			System.out.println(6 * i);
-			i = i + 1; // i++
-		}
+//		int i = 1;
+//		while(i < 10) {
+//			System.out.println(6 * i);
+//			i = i + 1; // i++
+//		}
 		
 		// 7단
-		for(int j = 1; j < 10; j++) {
-			System.out.println(7 * j);
-		}
+//		for(int j = 1; j < 10; j++) {
+//			System.out.println(7 * j);
+//		}
 		// while과 for 반복문 이용해 간단하게 만들수있다. 
 		
 		
