977cda98674c46be58c3856bc5063d24197cc835
iseung-u <iseung-u@192.168.0.4>
2021-02-28 16:06:38 +0900

    Loop

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -3,6 +3,20 @@ import java.util.Scanner;
 
 public class Gugudan {
 	public static void main(String[] args) {
+		// 6단 
+		int i = 1;
+		while(i < 10) {
+			System.out.println(6 * i);
+			i = i + 1; // i++
+		}
+		
+		// 7단
+		for(int j = 1; j < 10; j++) {
+			System.out.println(7 * j);
+		}
+		// while과 for 반복문 이용해 간단하게 만들수있다. 
+		
+		
 		// 2단
 //		System.out.println("2단");
 //		System.out.println(2 * 1);
@@ -48,19 +62,22 @@ public class Gugudan {
 //		result = 4 * 9;
 //		System.out.println(result);
 		
-		System.out.println("구구단 중 출력할 단은? : ");
-		Scanner scanner = new Scanner(System.in);
-		int number = scanner.nextInt();
-		System.out.println(number * 1);
-		System.out.println(number * 2);
-		System.out.println(number * 3);
-		System.out.println(number * 4);
-		System.out.println(number * 5);
-		System.out.println(number * 6);
-		System.out.println(number * 7);
-		System.out.println(number * 8);
-		System.out.println(number * 9);
+//		System.out.println("구구단 중 출력할 단은? : ");
+//		Scanner scanner = new Scanner(System.in);
+//		int number = scanner.nextInt();
+//		System.out.println(number * 1);
+//		System.out.println(number * 2);
+//		System.out.println(number * 3);
+//		System.out.println(number * 4);
+//		System.out.println(number * 5);
+//		System.out.println(number * 6);
+//		System.out.println(number * 7);
+//		System.out.println(number * 8);
+//		System.out.println(number * 9);
 		// 변수가 무었인지 정의 해보자.
+		
+		
+		
 	}
 
 }
